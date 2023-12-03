b10db17d707fa33e509afcc59e7f02c8645f95be
MikkelYtting <62931295+MikkelYtting@users.noreply.github.com>
2023-12-03 02:55:42 +0100

    Yes sir

---
+++ WebShopRestService-IntegrationTest/AddressTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using WebShopRestService.Data;
+using WebShopRestService.Repositories;
+
+[TestClass]
+public class AddressRepositoryIntegrationTests
+{
+    private MyDbContext _context;
+    private AddressRepository _addressRepository;
+
+    // This method will run before each test method is executed.
+    [TestInitialize]
+    public void TestSetup()
+    {
+        // Configure the DbContext with the connection string for the test database
+        var options = new DbContextOptionsBuilder<MyDbContext>()
+            .UseSqlServer("Server=tcp:mikkelyttingserver.database.windows.net,1433;Initial Catalog=DatabaseForUdviklere-Webshop;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication=Active Directory Default;")
+            .Options;
+
+        _context = new MyDbContext(options);
+
+        // Initialize the repository with the context
+        _addressRepository = new AddressRepository(_context);
+    }
+
+    [TestMethod]
+    public async Task AddressShouldNotExist_WhenQueriedWithNonExistentId()
+    {
+        // Arrange - ID set to 1 for testing or any other non-existent ID
+        int nonExistentAddressId = 1;
+
+        // Act
+        bool exists = await _addressRepository.AddressExistsAsync(nonExistentAddressId);
+
+        // Assert - Check that exists is false, meaning the address does not exist
+        Assert.IsFalse(exists, "Address with ID 1 should not exist.");
+    }
+
+    // This method will run after each test method is executed.
+    [TestCleanup]
+    public void TestCleanup()
+    {
+        _context.Dispose();
+    }
+}
---
+++ WebShopRestService-UnitTest/MockTest/OrderTableManagerTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WebShopRestService.Interfaces;
+using WebShopRestService.Managers;
+using WebShopRestService.Models;
+
+namespace WebShopRestService_UnitTest.MockTest
+{
+    [TestClass]
+    public class OrderTableManagerTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public async Task ValidateAndAddOrderAsync_ThrowsException_IfCustomerDoesNotExist()
+        {
+            // Arrange
+            var mockCustomerRepo = new Mock<ICustomersRepository>();
+            mockCustomerRepo.Setup(repo => repo.Exists(It.IsAny<int>())).ReturnsAsync(false);
+            var mockAddressRepo = new Mock<IAddressesRepository>();
+            var mockOrderTableRepo = new Mock<IOrderTablesRepository>();
+            var mockProductRepo = new Mock<IProductsRepository>();
+
+            var manager = new OrderTablesManager(
+                mockCustomerRepo.Object,
+                mockAddressRepo.Object,
+                mockOrderTableRepo.Object,
+                mockProductRepo.Object
+            );
+
+            var order = new OrderTable { /* ... */ };
+
+            // Act
+            await manager.ValidateAndAddOrderAsync(order);
+
+            // The ExpectedException attribute indicates that an exception is expected
+        }
+    }
+}
