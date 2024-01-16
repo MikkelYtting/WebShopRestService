ca4924a812dfd85989e5a4a81ba627c108fc5e90
MikkelYtting <62931295+MikkelYtting@users.noreply.github.com>
2024-01-16 18:09:33 +0100

    Fixed mongo post

--- WebShopRestService/Controllers/MongoDB/AddressesController.cs
+++ WebShopRestService/Controllers/MongoDB/AddressesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebShopRestService.Models.MongoDB;
+using WebShopRestService.DTO.MongoDTO;
 
 namespace WebShopRestService.Controllers.MongoDB
 {
@@ -21,11 +22,37 @@ namespace WebShopRestService.Controllers.MongoDB
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody] AddressMongo address)
+        public async Task<IActionResult> Post([FromBody] AddressDTO addressDto)
         {
-            await _mongoDBService.CreateAsync(address);
-            return CreatedAtAction(nameof(Get), new { id = address.Id }, address);
+            // Map the DTO to the domain model (manually or by using a mapper like AutoMapper)
+            var addressMongo = new AddressMongo
+            {
+                // Assuming Id is not set because MongoDB will auto-generate it
+                Street = addressDto.Street,
+                City = addressDto.City,
+                PostalCode = addressDto.PostalCode,
+                Country = addressDto.Country
+            };
+
+            // Call the service to create the new address
+            await _mongoDBService.CreateAsync(addressMongo);
+
+            // Create a response DTO including the generated Id
+            var responseDto = new AddressDTO
+            {
+                //Id = addressMongo.Id, // MongoDB has generated the Id at this point
+                Street = addressMongo.Street,
+                City = addressMongo.City,
+                PostalCode = addressMongo.PostalCode,
+                Country = addressMongo.Country
+            };
+
+            // Return the created address with the generated Id
+            // The 'Get' method should be one that exists and can handle a GET request for a single address by Id
+            return CreatedAtAction(nameof(Get), new { id = addressMongo.Id }, responseDto);
         }
+
+
 
         [HttpPut("{id}")]
         public async Task<IActionResult> Update(string id, [FromBody] AddressMongo address)
